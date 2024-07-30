d2fbc828b6523593179913852468972a1f604c39
SaiefB <A.S.Bokor@outlook.com>
2024-07-30 19:42:01 +0100

    Logic to get the Human HumanChoice
    - added comments to js
    - added a function to acquire input value

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+// Random Number Generator
 let getComputerChoice = function getRandom() {
     return Math.random()*10;
 }
 
+// Assign computerChoice to be equivalent ot getComputerChoice()
 let computerChoice = getComputerChoice();
 
+// Function to assign number values to RPS values
 if (computerChoice <= 3.33) {
     computerChoice = "Rock"
 } else if (computerChoice > 3.33 && computerChoice < 6.66) {
     computerChoice = "Paper"
 } else {
     computerChoice = "Scissors"
-}
+};
 
+
+// Assigns user input to getHumanChoice()
 function getHumanChoice() {
-    
+    return prompt("Rock, Paper or Scissors?")
 }
 
-HumanChoice = prompt("Rock, Paper or Scissors?")
+console.log(getHumanChoice())
